0a61051a551c69823e7c9fdb81f119a35cd9c55d
Owen Garmire <owen.garmire@timu.com>
2024-12-17 08:00:05 -0800

    add revert crop button

--- lib/src/widget/circle_icon_button.dart
+++ lib/src/widget/circle_icon_button.dart
@@ -38,3 +38,53 @@ class InstaPickerCircleIconButton extends StatelessWidget {
     );
   }
 }
+
+class InstaPickerTextIconButton extends StatelessWidget {
+  const InstaPickerTextIconButton({
+    super.key,
+    required this.onTap,
+    required this.icon,
+    required this.theme,
+    required this.size,
+    required this.text,
+  });
+
+  final VoidCallback onTap;
+  final Widget icon;
+  final double size;
+  final ThemeData? theme;
+  final String? text;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: size,
+      child: ElevatedButton(
+        onPressed: onTap,
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(size / 2)),
+          padding: theme?.buttonTheme.padding,
+          backgroundColor: theme?.buttonTheme.colorScheme?.surface,
+          foregroundColor: theme?.iconTheme.color,
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FittedBox(child: icon),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Text(
+                text!,
+                style: TextStyle(
+                  color: theme?.colorScheme.primary,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
